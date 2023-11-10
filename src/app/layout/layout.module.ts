ef6ff4de760aec844e3646f1ad94ade800d78e4d
Trinh Hoang Khai <trinhhoangkhai@secure.janeto.com>
2023-11-10 14:30:54 +0700

    ui header

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,17 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout.router';
 import { LayoutComponent } from './layout.component';
-import { HomeModule } from '../home/home.module';
+import { HeaderComponent } from './header/header.component';
+import { MatSelectModule } from '@angular/material/select';
+import { TranslateModule } from '@ngx-translate/core';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { MoreComponent } from './more/more.component';
 
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    HeaderComponent,
+    AboutUsComponent,
+    ContactComponent,
+    MoreComponent
   ],
   imports: [
     CommonModule,
     LayoutRoutingModule,
-    HomeModule
+    MatSelectModule,
+    TranslateModule
   ]
 })
 export class LayoutModule { }
--- src/app/layout/layout.router.ts
+++ src/app/layout/layout.router.ts
@@ -1,24 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { MoreComponent } from './more/more.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [
-      // {
-      //   path: '',
-      //   redirectTo: `home`,
-      //   pathMatch: 'full'
-      // },
-      // {
-      //   path: `home`,
-      //   loadChildren: () => import('../home/home.module').then(m => m.HomeModule)
-      // },
+      {
+        path: '',
+        redirectTo: `home`,
+        pathMatch: 'full'
+      },
+      {
+        path: `home`,
+        loadChildren: () => import('../home/home.module').then(m => m.HomeModule)
+      },
       {
         path: `branch`,
         loadChildren: () => import('../branch/branch.module').then(m => m.BranchModule)
+      },
+      {
+        path: `posts`,
+        loadChildren: () => import('../posts/posts.module').then(m => m.PostsModule)
+      },
+      {
+        path: `about-us`,
+        component: AboutUsComponent
+      },
+      {
+        path: `contact`,
+        component: ContactComponent
+      },
+      {
+        path: `more`,
+        component: MoreComponent
       },
     ]
   }
